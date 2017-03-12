32134f3aa1b992071427e96a50fcd94098d69c8d
Nick Rossolatos <spidey_nr@hotmail.com>
2017-03-12 22:48:59 +0200

    add Slack alert responses from server, add name for nameless class

--- themes/greecejs-theme/source/js/index.js
+++ themes/greecejs-theme/source/js/index.js
@@ -2,10 +2,12 @@
 
   var form = document.getElementById("slack-invite-form")
   var emailInput = form.querySelector("input[type='text']")
+  var submitButton = form.querySelector("button[type='submit']")
 
   var slack = new Slack()
   slack.init({
     emailInput: emailInput,
-    form: form
+    form: form,
+    submitButton: submitButton
   })
 })()
